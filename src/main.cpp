d3c6239acd29f3c8dc062ce204ad4427d39170d9
Henrique Jorge Barateli <henriquejb194@gmail.com>
2025-09-02 18:26:23 -0300

    feat: create WAD file class

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <filesystem>
 #include <format>
+#include <unordered_map>
 
 
 class WadReader {
@@ -11,7 +12,7 @@ class WadReader {
 
 public:
     explicit WadReader(const std::filesystem::path& wad_file)
-        : wad(wad_file, std::ios::binary) {
+        : wad{wad_file, std::ios::binary} {
         wad.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     }
 
@@ -34,13 +35,13 @@ public:
     std::string readString(std::streamsize size) {
         std::vector<char> buffer(size);
         wad.read(buffer.data(), size);
+        // Remove any trailing zeroes.
         size_t i = 0;
         for (; i < size; i++) {
             if (buffer[i] == '\0') {
                 break;
             }
         }
-        buffer.erase(buffer.begin() + i, buffer.end());
         return {buffer.data(), i};
     }
 };
@@ -48,105 +49,113 @@ public:
 
 struct WadHeader {
     // The ASCII characters "IWAD" or "PWAD".
-    std::string id{};
+    std::string id;
 
     // An integer specifying the number of lumps in the WAD.
-    Sint32 num_lumps{};
+    Sint32 num_lumps;
 
     // An integer holding a pointer to the location of the directory.
-    Sint32 directory_ofs{};
+    Sint32 directory_ofs;
+
+    explicit WadHeader(WadReader& reader)
+        : id{reader.readString(4)}, num_lumps{reader.readInt()},
+          directory_ofs{reader.readInt()} {
+        if (id != "IWAD" && id != "PWAD") {
+            const auto error = "WAD contains invalid id";
+            throw std::domain_error(error);
+        }
+        if (num_lumps <= 0) {
+            const auto error = "WAD contains invalid number of lumps";
+            throw std::domain_error(error);
+        }
+        if (directory_ofs <= 0) {
+            const auto error = "WAD contains invalid directory offset";
+            throw std::domain_error(error);
+        }
+    }
 };
 
 struct WadLump {
     // An integer holding a pointer to the start of the lump's data in the file.
-    Sint32 position{};
+    Sint32 position;
 
     // An integer representing the size of the lump in bytes.
-    Sint32 size{};
+    Sint32 size;
 
     // An ASCII string defining the lump's name. The name has a limit
     // of 8 characters, the same as the main portion of an MS-DOS filename.
-    // The name must be nul-terminated if less than 8 characters; for maximum
-    // tool compatibility, it is best to pad any remainder with nul characters
-    // as well.
-    std::string name{};
+    std::string name;
+
+    explicit WadLump(WadReader& reader)
+        : position{reader.readInt()}, size{reader.readInt()},
+          name{reader.readString(8)} {
+        if (position < 0) {
+            const auto error = std::format(
+                "Lump \"{}\" contains invalid data offset!",
+                name
+            );
+            throw std::domain_error(error);
+        }
+        if (size < 0) {
+            const auto error = std::format(
+                "Lump \"{}\" contains invalid size!",
+                name
+            );
+            throw std::domain_error(error);
+        }
+    }
 };
 
-using WadDirectory = std::vector<WadLump>;
+class WadDirectory {
+    std::vector<WadLump> lumps;
+    std::unordered_map<std::string_view, Sint32> lump_map;
 
-static WadLump readLump(WadReader& reader) {
-    const auto position{reader.readInt()};
-    const auto size = reader.readInt();
-    auto name{reader.readString(8)};
-    if (position < 0) {
-        const auto error = std::format(
-            "Lump \"{}\" contains invalid data offset!",
-            name
-        );
-        throw std::domain_error(error);
-    }
-    if (size < 0) {
-        const auto error = std::format(
-            "Lump \"{}\" contains invalid size!",
-            name
-        );
-        throw std::domain_error(error);
-    }
-    return {
-        .position{position},
-        .size{size},
-        .name{std::move(name)}
-    };
-}
+public:
+    explicit WadDirectory(WadReader& reader, const WadHeader& header) {
+        const auto num_lumps{header.num_lumps};
+        reader.seek(header.directory_ofs);
+        lumps.reserve(num_lumps);
+        lump_map.reserve(num_lumps);
+        for (Sint32 i = 0; i < num_lumps; i++) {
+            lumps.emplace_back(reader);
+            lump_map.try_emplace(lumps[i].name, i);
 
-static WadDirectory readDirectory(WadReader& reader, const WadHeader& header) {
-    // We can't use a map, because multiple lumps may have the same name.
-    reader.seek(header.directory_ofs);
-    WadDirectory directory(header.num_lumps);
-    for (int i = 0; i < header.num_lumps; i++) {
-        directory[i] = readLump(reader);
+            std::cout << "WadLump {" << std::endl;
+            std::cout << "  position: " << lumps[i].position << std::endl;
+            std::cout << "  size: " << lumps[i].size << std::endl;
+            std::cout << "  name: " << lumps[i].name << std::endl;
+            std::cout << "}" << std::endl;
+        }
     }
-    return directory;
-}
 
-static WadHeader readHeader(WadReader& reader) {
-    auto id{reader.readString(4)};
-    const auto num_lumps{reader.readInt()};
-    const auto directory_ofs{reader.readInt()};
-    if (id != "IWAD" && id != "PWAD") {
-        const auto error = "WAD contains invalid id";
-        throw std::domain_error(error);
+    [[nodiscard]]
+    std::optional<Sint32> searchLump(std::string_view lump_name) const {
+        const auto lump_index = lump_map.find(lump_name);
+        if (lump_index == lump_map.end()) {
+            return std::nullopt;
+        }
+        return lump_index->second;
     }
-    if (num_lumps <= 0) {
-        const auto error = "WAD contains invalid number of lumps";
-        throw std::domain_error(error);
-    }
-    if (directory_ofs <= 0) {
-        const auto error = "WAD contains invalid directory offset";
-        throw std::domain_error(error);
-    }
-    return {
-        .id{std::move(id)},
-        .num_lumps{num_lumps},
-        .directory_ofs{directory_ofs},
-    };
-}
+};
 
+class WadFile {
+    WadReader reader;
+    WadHeader header;
+    WadDirectory directory;
+
+public:
+    explicit WadFile(const std::filesystem::path& wad_file)
+        : reader{wad_file}, header{reader}, directory{reader, header} {
+    }
+
+    [[nodiscard]]
+    std::optional<Sint32> searchLump(std::string_view lump_name) const {
+        return directory.searchLump(lump_name);
+    }
+};
 
 int main(int argc, char* argv[]) {
-    const auto filename = "doom.wad";
-    WadReader reader(filename);
-
-    const WadHeader header{readHeader(reader)};
-    const WadDirectory directory{readDirectory(reader, header)};
-
-    for (const auto& [position, size, name] : directory) {
-        std::cout << "WadLump {" << std::endl;
-        std::cout << "  position: " << position << std::endl;
-        std::cout << "  size: " << size << std::endl;
-        std::cout << "  name: " << name << std::endl;
-        std::cout << "}" << std::endl;
-    }
-
+    const auto filename{"doom.wad"};
+    WadFile wad{filename};
     return EXIT_SUCCESS;
 }
